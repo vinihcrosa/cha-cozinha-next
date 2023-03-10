d280fc2d17f8a4f15c3cf44a688e34a6898bb04c
vinihcrosa <vinihcrosa@users.noreply.github.com>
2023-03-10 02:43:49 -0300

    feat: selecao de presente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,39 +4,97 @@ import { Inter } from 'next/font/google'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-import './styles.module.css'
+import styles from './styles.module.css'
+import { render } from 'react-dom'
 
 const inter = Inter({ subsets: ['latin'] })
 const url = process.env.URL || 'http://localhost:3000'
 
 type Presente = {
-  _id: number,
+  _id: string,
   nome: string,
   presente: string,
 }
 
 export default function Home({data}: { data: Presente[]}) {
   const [presentes, setPresentes] = useState<Presente[]>(data)
+  const [presentesSelecionado, setPresentesSelecionado] = useState<string[]>([])
+  const [totalPresentes, setTotalPresentes] = useState<number>(0)
+  console.log('renderizou')
+  
+  function handleSelecionaPresente(id: string) {
+    const isPresenteSelecionado = presentesSelecionado.includes(id)
+    console.log(id, " -> ", isPresenteSelecionado)
+    if (isPresenteSelecionado) {
+      presentesSelecionado.splice(presentesSelecionado.indexOf(id), 1)
+      setPresentesSelecionado(presentesSelecionado)
+      setTotalPresentes(totalPresentes - 1)
+    } else {
+      presentesSelecionado.push(id)
+      setPresentesSelecionado(presentesSelecionado)
+      setTotalPresentes(totalPresentes + 1)
+    }
+    console.log(presentesSelecionado)
+  }
+
+
+
+  useEffect(() => {
+    console.log('useEffect')
+    console.log(presentesSelecionado)
+  }, [presentesSelecionado, totalPresentes])
+
+  function isPresenteSelecionado(id: string) {
+    return presentesSelecionado.includes(id)
+  }
 
   return (
-    <>
-      <h1>Cha de Casa Nova</h1>
-      <ul id="myUL">
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>Cha de Casa Nova</h1>
+        <span>Um novo Lar</span>
+      </div>
+
+      <div className={styles.nameContainer}>
+        <span>Coloque o seu nome aqui</span>
+        <input type="text" placeholder="Nome" />
         {
-          presentes.map((presente) => (
-            <li key={presente._id}>
-              {presente.presente}
-            </li>
-          ))
-        }
-        <li>Hit the gym</li>
-        <li className="checked">Pay bills</li>
-        <li>Meet George</li>
-        <li>Buy eggs</li>
-        <li>Read a book</li>
-        <li>Organize office</li>
-      </ul>
-    </>
+            presentes.map((presente) => {
+              if(!presentesSelecionado.includes(presente._id)) return null
+              return (
+                <div key={presente._id} className={styles.presente}>
+                  <div className={styles.presenteName}>{presente.presente}</div>
+                  <button 
+                    key={presente._id} 
+                    onClick={() => {
+                    handleSelecionaPresente(presente._id)
+                    }}
+                    className={ styles.btnPresente }
+                  >Retirar</button>
+                </div>
+              )
+            })
+          }
+      </div>
+
+      <div className={styles.presentContainer}>
+          {
+            presentes.map((presente) => (
+              !isPresenteSelecionado(presente._id) &&
+              <div key={presente._id} className={styles.presente}>
+                <div className={styles.presenteName}>{presente.presente}</div>
+                <button 
+                  key={presente._id} 
+                  onClick={() => {
+                  handleSelecionaPresente(presente._id)
+                  }}
+                  className={ styles.btnPresente }
+                >Selecionar</button>
+              </div>
+            ))
+          }
+      </div>
+    </div>
   )
 }
 
